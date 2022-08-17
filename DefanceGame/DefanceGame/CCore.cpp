dacda498a98fa2a6db0044f453f0a105c234b4f5
Lim DongHyen <99638155+DongDongLim@users.noreply.github.com>
2022-08-17 12:40:40 +0900

    feat : Set MemoryDC

--- DefanceGame/DefanceGame/CCore.cpp
+++ DefanceGame/DefanceGame/CCore.cpp
@@ -6,12 +6,23 @@ CCore::CCore()
 
 CCore::~CCore()
 {
-
+	ReleaseDC(hWnd, m_hDC);// 사용한 dc 반납
+	DeleteDC(m_hMemDC);
+	DeleteObject(m_hBMP);
 }
+
+POINT g_rectPos;
 
 void CCore::init()
 {
-
+	// 코어의 초기화 과정
+	m_hDC = GetDC(hWnd); // 그려야하는 윈도우의 dc를 가져오는 방법
+	m_hMemDC = CreateCompatibleDC(m_hDC);
+	m_hBMP = CreateCompatibleBitmap(m_hDC, WINSIZEX, WINSIZEY);
+	HBITMAP hOldBitmap = (HBITMAP)SelectObject(m_hMemDC, m_hBMP);
+	DeleteObject(hOldBitmap);
+	g_rectPos.x = 300;
+	g_rectPos.y = 300;
 }
 
 void CCore::update()
