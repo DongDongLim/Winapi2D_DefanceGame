42769aa3a631b982602452e1075f700526849f4d
Lim DongHyen <99638155+DongDongLim@users.noreply.github.com>
2022-08-17 10:41:26 +0900

    feat : Singleton Macro

---
+++ DefanceGame/DefanceGame/USingleTon.h
@@ -0,0 +1,16 @@
+#pragma once
+
+#define SINGLETON(CLASS)			\
+									\
+private:							\
+	CLASS()	{};						\
+	~CLASS(){};						\
+									\
+public:								\
+	static	CLASS* getInst()		\
+	{								\
+		static CLASS _instance;		\
+		return &_instance;			\
+	}								\
+private:
+
